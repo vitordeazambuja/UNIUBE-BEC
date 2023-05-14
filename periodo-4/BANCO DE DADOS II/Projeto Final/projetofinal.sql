7885a779fb4cc09cfad5c6826ae1c52bbbc46163
Vitor <vitordeazambuja@edu.uniube.br>
2023-05-14 19:50:38 -0300

    Terminei o projeto final parte 2 e adicionei 1 slide de poo

--- periodo-4/BANCO DE DADOS II/Projeto Final/projetofinal.sql
+++ periodo-4/BANCO DE DADOS II/Projeto Final/projetofinal.sql
@@ -17,6 +17,7 @@ CREATE TABLE livro(
 id_livro int not null,
 editora_id int not null,
 titulo varchar(125) not null,
+genero varchar(125) not null,
 autor varchar(125) not null,
 PRIMARY KEY (id_livro),
 FOREIGN KEY (editora_id) REFERENCES editora(id_editora)
@@ -125,11 +126,11 @@ VALUES
 (5,'Lucas Costa', 'Rua da Paz, 234, Vila Mariana, São Paulo - SP');
 INSERT INTO editora
 VALUES
-(1,'Livros & Cia','Rua dos Livreiros, 123, Centro, São Paulo - SP'),
-(2,'Edições Literárias','Avenida dos Escritores, 456, Jardim Botânico, Rio de Janeiro - RJ'),
-(3,'Editora Criativa','Rua dos Artistas, 789, Vila Mariana, São Paulo - SP'),
-(4,'Livraria do Saber','Praça da Cultura, 101, Savassi, Belo Horizonte - MG'),
-(5,'Casa das Letras','Avenida das Letras, 234, Jardim América, Belo Horizonte - MG');
+(1,'Companhia das Letras','Rua Bandeira Paulista, 702, cj.32, 04532-002 São Paulo - SP'),
+(2,'Darkside Books','Av. Portugal, 46 - Galpão 1 - Itaqui - Itapevi - SP'),
+(3,'Editora Aleph','Rua Tabapuã, 81 – Conj. 134, Itaim Bibi – São Paulo/SP'),
+(4,'Globo Livros','Rua Marquês de Pombal, 25 – 2º andar – Centro – Rio de Janeiro – RJ'),
+(5,'Editora Intrínseca','Rua Marquês de São Vicente, 99 / 6º andar Gávea - Rio de Janeiro - RJ');
 INSERT INTO fornecedor
 VALUES
 (1,'Livraria das Letras','Rua do Saber, 456, Centro, Rio de Janeiro - RJ','contato@livrariadasletras.com.br'),
@@ -144,15 +145,34 @@ VALUES
 (3,'Pedro Almeida','456.789.123-00','Rua das Árvores, 789, Vila Madalena, Belo Horizonte - MG',3200),
 (4,'Luana Silva','789.123.456-00','Av. das Flores, 123, Centro, Florianópolis - SC',2800),
 (5,'Carlos Vieira','654.321.987-00','Rua das Pedras, 789, Centro, Brasília - DF',5000);
+INSERT INTO livro
+VALUES
+(1,1,'Um Verão Italiano', 'Romance', 'Rebecca Serle'),
+(2,1,'Oito Horas Perfeitas', 'Romance', 'Lia Louis'),
+(3,1,'João e Maria', 'Infantil','Hervé Le Goff'),
+(4,2,'O Demonologiasta', 'Terror', 'Gillian Flynn'),
+(5,2,'Evangelho de Sangue', 'Terror', 'Clive Barker'),
+(6,2,'Fragmentos do Horror', 'Terror', 'Junji Ito'),
+(7,3,'1984', 'Ficção', 'George Orwell'),
+(8,3,'Blade Runner', 'Ficção', 'Philip K. Dick'),
+(9,3,'A Revolução dos Bichos', 'Ficção', 'George Orwell'),
+(10,4,'O Iglu', 'Infantil', 'Flávia Lins e Silva'),
+(11,4,'A Mecânica do Amor', 'Romance', 'Alexene Farol Follmuth'),
+(12,4,'Minhas Mãos', 'Infantil', 'Néjib'),
+(13,5,'A Culpa é Das Estrelas', 'Romance', 'John Green'),
+(14,5,'Vovê deu no pé', 'Infantil', 'David Williams'),
+(15,5,'Terra Faminta', 'Terror', 'Andrew Michael Hurley');
 
-/* POPULANDO A TABELA COM WHILE*/
+/* POPULANDO A TABELA COM WHILE
 DECLARE @contador INT = 1;
 WHILE @contador <= 20
 BEGIN
 	INSERT INTO livro
-	VALUES (@contador, FLOOR(RAND()*5)+1,'Livro '+CAST(@contador AS VARCHAR),'Autor '+CAST(@contador AS VARCHAR));
+	VALUES (@contador, FLOOR(RAND()*5)+1,'Livro '+CAST(@contador AS VARCHAR),'Genero ' +CAST(@contador AS VARCHAR),'Autor '+CAST(@contador AS VARCHAR));
 	SET @contador += 1;
 END
+*/
+
 /* TESTES COM SELECT ALL */
 SELECT * FROM cliente;
 SELECT * FROM editora;
@@ -178,10 +198,10 @@ VALUES
 (1,1,'30-03-23',0);
 INSERT INTO item_pedido
 VALUES
-(1,2,1,1,20);
+(1,2,1,1,50.93);
 INSERT INTO item_pedido
 VALUES
-(2,3,1,2,40);
+(2,3,1,2,48.91);
 select * from item_pedido;
 select * from pedido;
 /* TRIGGER PARA ATUALIZAR O VALOR TOTAL DA COMPRA QUANDO FOR INSERIDO UM ELEMENTO NA TABELA item_compra */
@@ -205,16 +225,16 @@ VALUES
 (4,3,'30-03-23',0);
 INSERT INTO item_compra
 VALUES
-(1,1,1,1,200);
+(1,1,1,1,40.93);
 INSERT INTO item_compra
 VALUES
-(2,2,2,2,400);
+(2,2,2,2,38.91);
 INSERT INTO item_compra
 VALUES
-(3,3,3,3,350);
+(3,3,3,3,32.83);
 INSERT INTO item_compra
 VALUES
-(4,4,4,2,500);
+(4,4,4,2,23.90);
 select * from item_compra;
 select * from compra;
 /* IF CONDICIONAL */
@@ -238,7 +258,7 @@ BEGIN
 	SET @contador += 1;
 END
 /* FUNÇÃO IIF PARA BUSCAR LIVROS DE UMA DETERMINADA EDITORA*/
-SELECT id_livro,titulo,autor, IIF(editora_id = 5, 'livro da Casa das Letras','livro de outra editora')
+SELECT id_livro,titulo,autor, IIF(editora_id = 5, 'livro da Editora Intrínseca','livro de outra editora')
 FROM livro
 /* DEMONSTRAÇÃO DE CASE/WHEN */
 SELECT c.id_compra,
@@ -341,17 +361,231 @@ RETURNS INT
 AS
 BEGIN
     DECLARE @quantidade_livros INT
-
     SELECT @quantidade_livros = SUM(quantidade)
     FROM item_compra
     WHERE livro_id = @id_livro
-
     RETURN @quantidade_livros
 END
 GO
-
 INSERT INTO item_compra
-VALUES(5, 1, 1, 2, 200);
-
+VALUES(5, 1, 1, 2, 40.93);
 SELECT dbo.qtde_livros_disponiveis(1) AS 'Quantidade de livros disponíveis'
 SELECT * FROM item_compra
+
+-- MINERAÇÃO DE DADOS
+SELECT * FROM pedido;
+SELECT * FROM item_pedido;
+/* CRIAÇÃO DE PEDIDOS */
+INSERT INTO pedido
+VALUES
+(2,1,'01-04-23',0),
+(3,2,'02-04-23',0),
+(4,3,'03-04-23',0),
+(5,4,'04-04-23',0),
+(6,5,'05-04-23',0),
+(7,1,'06-04-23',0),
+(8,2,'07-04-23',0),
+(9,3,'08-04-23',0),
+(10,4,'09-04-23',0),
+(11,5,'10-04-23',0),
+(12,1,'11-04-23',0),
+(13,2,'12-04-23',0),
+(14,3,'13-04-23',0),
+(15,4,'14-04-23',0),
+(16,5,'15-04-23',0),
+(17,1,'16-04-23',0);
+/* CRIAÇÃO ITEM PEDIDOS */
+--CARRINHO 2
+INSERT INTO item_pedido
+VALUES
+(3,1,2,1,50.93);
+INSERT INTO item_pedido
+VALUES
+(4,4,2,1,33.90);
+--CARRINHO 3
+INSERT INTO item_pedido
+VALUES
+(5,7,3,1,14.90);
+INSERT INTO item_pedido
+VALUES
+(6,4,3,1,33.90);
+--CARRINHO 4
+INSERT INTO item_pedido
+VALUES
+(7,3,4,1,42.83);
+--CARRINHO 5
+INSERT INTO item_pedido
+VALUES
+(8,2,5,1,48.91);
+INSERT INTO item_pedido
+VALUES
+(9,10,5,1,61.60);
+--CARRINHO 6
+INSERT INTO item_pedido
+VALUES
+(10,5,6,1,40.99);
+--CARRINHO 7
+INSERT INTO item_pedido
+VALUES
+(11,8,7,1,44.16);
+INSERT INTO item_pedido
+VALUES
+(12,6,7,1,43.90);
+--CARRINHO 8
+INSERT INTO item_pedido
+VALUES
+(13,11,8,1,49.90);
+INSERT INTO item_pedido
+VALUES
+(14,12,8,1,72.08);
+--CARRINHO 9
+INSERT INTO item_pedido
+VALUES
+(15,13,9,1,30.99);
+--CARRINHO 10
+INSERT INTO item_pedido
+VALUES
+(16,9,10,1,11.70);
+--CARRINHO 11
+INSERT INTO item_pedido
+VALUES
+(17,7,11,1,14.90);
+INSERT INTO item_pedido
+VALUES
+(18,15,11,1,44.99);
+--CARRINHO 12
+INSERT INTO item_pedido
+VALUES
+(19,14,12,1,49);
+--CARRINHO 13
+INSERT INTO item_pedido
+VALUES
+(20,7,13,1,14.90);
+INSERT INTO item_pedido
+VALUES
+(21,4,13,1,33.90);
+--CARRINHO 14
+INSERT INTO item_pedido
+VALUES
+(22,5,14,1,40.99);
+INSERT INTO item_pedido
+VALUES
+(23,3,14,1,42.83);
+--CARRINHO 15
+INSERT INTO item_pedido
+VALUES
+(24,1,15,1,50.93);
+INSERT INTO item_pedido
+VALUES
+(25,10,15,1,61.60);
+--CARRINHO 16
+INSERT INTO item_pedido
+VALUES
+(26,6,16,1,43.90);
+--CARRINHO 17
+INSERT INTO item_pedido
+VALUES
+(27,2,17,1,48.91);
+INSERT INTO item_pedido
+VALUES
+(28,12,17,1,72.08);
+
+/* SUPORTE E CONFIANÇA */
+
+--FICÇÃO E TERROR
+--QUANTIDADE DE LIVROS DE FICÇÃO
+DECLARE @qntlivrofic int;
+SET @qntlivrofic = (
+SELECT COUNT(itp.id_item_pedido) AS 'Carrinhos com livros de Ficção'
+FROM item_pedido itp
+INNER JOIN livro l
+ON itp.livro_id = l.id_livro
+WHERE l.genero = 'Ficção'
+);
+PRINT('Sup(Ficção) = '+CAST(@qntlivrofic AS VARCHAR));
+-- 5 CARRINHOS COM LIVROS DE FICÇÃO
+
+--QUANTIDADE DE LIVROS DE TERROR
+DECLARE @qntlivroterror int;
+SET @qntlivroterror = (
+SELECT COUNT(itp.id_item_pedido) AS 'Carrinhos com livros de Terror'
+FROM item_pedido itp
+INNER JOIN livro l
+ON itp.livro_id = l.id_livro
+WHERE l.genero = 'Terror'
+);
+PRINT('Sup(Terror) = '+CAST(@qntlivroterror AS VARCHAR));
+-- 8 CARRINHOS COM LIVROS DE TERROR
+
+--QUANTIDADE DE CARRINHOS COM LIVROS DE FICÇÃO E TERROR JUNTOS
+DECLARE @qntftjuntos int;
+SET @qntftjuntos = ( 
+SELECT COUNT(*) AS 'Carrinhos com livros de Ficção e Terror'
+FROM (
+  SELECT pedido_id
+  FROM item_pedido
+  WHERE livro_id IN (
+    SELECT id_livro
+    FROM livro
+    WHERE genero IN ('Ficção', 'Terror')
+  )
+  GROUP BY pedido_id
+  HAVING COUNT(DISTINCT livro_id) = 2
+) AS a
+);
+PRINT('Sup({Ficção} -> {Terror}) = '+ CAST(@qntftjuntos AS VARCHAR));
+-- 4 CARRINHOS COM LIVROS DE FICÇÃO E TERROR JUNTOS
+
+-- REGRA DE ASSOCIAÇÃO {Ficção} -> {Terror}
+DECLARE @confficterror DECIMAL(10,2);
+SET @confficterror = ((@qntftjuntos*1.0/@qntlivrofic)*100);
+PRINT('Conf({Ficção} -> {Terror}) = '+ CAST(@confficterror AS VARCHAR)  +'%');
+DECLARE @confterrorfic DECIMAL(10,2);
+SET @confterrorfic = ((@qntftjuntos*1.0/@qntlivroterror)*100);
+PRINT('Conf({Terror} -> {Ficção}) = '+ CAST(@confterrorfic AS VARCHAR) +'%');
+
+--ROMANCE E INFANTIL
+--QUANTIDADE DE LIVROS DE ROMANCE
+DECLARE @qntlivrorom INT;
+SET @qntlivrorom = (
+SELECT COUNT(itp.id_item_pedido) AS 'Carrinhos com livros de Romance'
+FROM item_pedido itp
+INNER JOIN livro l
+ON itp.livro_id = l.id_livro
+WHERE l.genero = 'Romance'
+);
+PRINT('Sup(Romance) = '+ CAST(@qntlivrorom AS VARCHAR));
+--QUANTIDADE DE LIVROS INFANTIS
+DECLARE @qntlivroinf INT
+SET @qntlivroinf = (
+SELECT COUNT(itp.id_item_pedido) AS 'Carrinhos com livros Infantis'
+FROM item_pedido itp
+INNER JOIN livro l
+ON itp.livro_id = l.id_livro
+WHERE l.genero = 'Infantil'
+);
+PRINT('Sup(Infantil) = '+ CAST(@qntlivroinf AS VARCHAR));
+--QUANTIDADE DE CARRINHOS COM LIVROS DE ROMANCE E INFANTIL JUNTOS
+DECLARE @qntrijuntos INT
+SET @qntrijuntos = (
+SELECT COUNT(*) AS 'Carrinhos com livros de Romance e Infantis'
+FROM (
+  SELECT pedido_id
+  FROM item_pedido
+  WHERE livro_id IN (
+    SELECT id_livro
+    FROM livro
+    WHERE genero IN ('Romance', 'Infantil')
+  )
+  GROUP BY pedido_id
+  HAVING COUNT(DISTINCT livro_id) = 2
+) AS a
+);
+PRINT('Sup({Romance} -> {Infantil}) = '+ CAST(@qntrijuntos AS VARCHAR));
+-- REGRA DE ASSOCIAÇÃO {Romance} -> {Infantil}
+DECLARE @confrominfantil DECIMAL(10,2);
+SET @confrominfantil = ((@qntrijuntos*1.0/@qntlivrorom)*100);
+PRINT('Conf({Romance} -> {Infantil}) = '+ CAST(@confrominfantil AS VARCHAR)  +'%');
+DECLARE @confinfromance DECIMAL(10,2);
+SET @confinfromance = ((@qntrijuntos*1.0/@qntlivroinf)*100);
+PRINT('Conf({Infantil} -> {Romance}) = '+ CAST(@confinfromance AS VARCHAR) +'%');
