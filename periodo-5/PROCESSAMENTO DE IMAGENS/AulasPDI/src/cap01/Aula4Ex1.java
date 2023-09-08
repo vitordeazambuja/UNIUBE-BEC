a66c374f0eae32dbb3999463961d008c17ac8a11
Vitor <vitordeazambuja@edu.uniube.br>
2023-09-07 21:25:49 -0300

    Comecei o exercicio 1 de PDI

--- periodo-5/PROCESSAMENTO DE IMAGENS/AulasPDI/src/cap01/Aula4Ex1.java
+++ periodo-5/PROCESSAMENTO DE IMAGENS/AulasPDI/src/cap01/Aula4Ex1.java
@@ -1,4 +1,27 @@
 package cap01;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
 
 public class Aula4Ex1 {
+    public static void main (String [] args) throws Exception{
+        int width = 100;
+        int height = 100;
+        BufferedImage buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_BGR);
+        Graphics g = buffer.createGraphics();
+        g.setColor(Color.BLUE);
+        g.fillRect(-50,-50,width,height);
+        g.setColor(Color.GREEN);
+        g.fillRect(50,-50,width,height);
+        g.setColor(Color.RED);
+        g.fillRect(-50,50,width,height);
+        g.setColor(Color.YELLOW);
+        g.fillRect(50,50, width,height);
+        g.setColor(Color.WHITE);
+        g.drawLine(-100,50,100,50);
+        g.drawLine(50,-100,50,100);
+        ImageIO.write(buffer, "PNG", new File("ex1.png"));
+    }
 }
