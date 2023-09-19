6bad36b5cee6ab035c130550ccc0356aa8a428be
Vitor <vitordeazambuja@gmail.com>
2023-09-19 20:16:50 -0300

    Mudei o arquivo CMake do CLion

---
+++ periodo-5/ESTRUTURA DE DADOS I/AulasED1/cmake-build-debug/CMakeFiles/listaencadeada.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/listaencadeada.dir/listaencadeada.c.obj"
+  "CMakeFiles/listaencadeada.dir/listaencadeada.c.obj.d"
+  "liblistaencadeada.dll.a"
+  "listaencadeada.exe"
+  "listaencadeada.exe.manifest"
+  "listaencadeada.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/listaencadeada.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
