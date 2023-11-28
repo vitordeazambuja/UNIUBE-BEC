8f7be8b72c859d0f2ddf2d2f85d0ff76280999ee
Vitor <vitordeazambuja@edu.uniube.br>
2023-11-28 18:40:24 -0300

    Terminei projeto basico SC

--- periodo-5/SISTEMAS DE CONTROLE/seminario_front_end/src/App.jsx
+++ periodo-5/SISTEMAS DE CONTROLE/seminario_front_end/src/App.jsx
@@ -1,7 +1,40 @@
+import {useState} from "react";
+import './styles/app.css'
+
 function App() {
 
+  // Declaração de variáveis
+  const [counter, setCounter] = useState(0)
+
+  // Métodos
+  const increase = () => {
+    setCounter(count => count + 1)
+  }
+
+  const decrease = () => {
+    if (counter > 0){
+      setCounter(count => count -1)
+    }
+  }
+
+  const reset = () => {
+    setCounter(0)
+  }
+
   return (
-      <p>Hello World</p>
+      <div className="App">
+        <h1>SISTEMAS DE CONTROLE</h1>
+        <p>AUTOMATIZAÇÃO DE PROCESSOS NO DESENVOLVIMENTO WEB FRONT-END</p>
+
+        <h2 className="counter-title">Contador: {counter}</h2>
+
+        <div className="btn_container">
+          <button onClick={increase}>Aumentar</button>
+          <button onClick={decrease}>Diminuir</button>
+          <button onClick={reset}>Resetar</button>
+        </div>
+
+      </div>
   )
 }
 
