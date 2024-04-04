88f5ccd11d41a010d184b5c0e695394ca49c77de
Vitor <vitordeazambuja@edu.uniube.br>
2024-04-04 18:56:29 -0300

    Traduzido codigo

--- periodo-6/ESTRUTURA DE DADOS II/sorting-algorithms/bubbleSort.py
+++ periodo-6/ESTRUTURA DE DADOS II/sorting-algorithms/bubbleSort.py
@@ -3,49 +3,50 @@ import random
 import matplotlib.pyplot as plt
 
 def bubbleSort(v):
-    comparisons = 0
-    movements = 0
+    comparacoes = 0
+    movimentos = 0
     for i in range(len(v) - 1):
         for j in range(0, len(v) - 1 - i):
-            comparisons += 1
+            comparacoes += 1
             if v[j] > v[j + 1]:
                 aux = v[j]
                 v[j] = v[j + 1]
                 v[j + 1] = aux
-                movements += 1
-    return comparisons, movements
+                movimentos += 1
+    return comparacoes, movimentos
 
-def measure_execution_time(array):
-    start_time = time.time()
-    comparisons, movements = bubbleSort(array)
-    end_time = time.time()
-    return end_time - start_time, comparisons, movements
+def medir_tempo(array):
+    tempo_inicial = time.time()
+    comparacoes, movimentos = bubbleSort(array)
+    tempo_final = time.time()
+    return tempo_final - tempo_inicial, comparacoes, movimentos
 
-def plot_execution_times(sizes, times, title, labels):
+def plotar_grafico(tamanhos, times, title, legendas):
     for i in range(len(times)):
-        plt.plot(sizes, times[i], marker='o', label=labels[i])
+        plt.plot(tamanhos, times[i], marker='o', label=legendas[i])
     plt.title(title)
     plt.xlabel('Tamanho da Array')
     plt.ylabel('Tempo de Execução (segundos)')
     plt.grid(True)
     plt.legend()
 
-sizes = [100, 1000, 10000]
-execution_times = [[], [], []]
-labels = ['Crescente', 'Decrescente', 'Aleatória']
+tamanhos = [100, 1000, 10000]
+ordens = [[], [], []]
+legendas = ['Crescente', 'Decrescente', 'Aleatória']
 
-for size in sizes:
-    ascending_array = list(range(size))
-    descending_array = list(range(size, 0, -1))
-    random_array = random.sample(range(size * 10), size)
+for size in tamanhos:
+    array_cresc = list(range(size))
+    array_decresc = list(range(size, 0, -1))
+    array_random = random.sample(range(size * 10), size)
 
-    time_asc, _, _ = measure_execution_time(ascending_array)
-    time_desc, _, _ = measure_execution_time(descending_array)
-    time_rand, _, _ = measure_execution_time(random_array)
+    tempo_asc, _, _ = medir_tempo(array_cresc)
+    tempo_desc, _, _ = medir_tempo(array_decresc)
+    tempo_rand, _, _ = medir_tempo(array_random)
 
-    execution_times[0].append(time_asc)
-    execution_times[1].append(time_desc)
-    execution_times[2].append(time_rand)
+    ordens[0].append(tempo_asc)
+    ordens[1].append(tempo_desc)
+    ordens[2].append(tempo_rand)
 
-plot_execution_times(sizes, execution_times, 'Tempo de execução para diferentes ordens de arrays', labels)
+plotar_grafico(tamanhos, ordens, 'Tempo de execução para diferentes ordens de arrays', legendas)
+
 plt.show()
