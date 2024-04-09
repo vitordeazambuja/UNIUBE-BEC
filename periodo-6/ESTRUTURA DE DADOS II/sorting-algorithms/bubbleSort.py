ff45687a504de61e20e6a247b6348b3994f4c3fc
Vitor <vitordeazambuja@edu.uniube.br>
2024-04-09 13:03:47 -0300

    Atualizado o codigo

--- periodo-6/ESTRUTURA DE DADOS II/sorting-algorithms/bubbleSort.py
+++ periodo-6/ESTRUTURA DE DADOS II/sorting-algorithms/bubbleSort.py
@@ -3,23 +3,20 @@ import random
 import matplotlib.pyplot as plt
 
 def bubbleSort(v):
-    comparacoes = 0
-    movimentos = 0
     for i in range(len(v) - 1):
         for j in range(0, len(v) - 1 - i):
-            comparacoes += 1
             if v[j] > v[j + 1]:
                 aux = v[j]
                 v[j] = v[j + 1]
                 v[j + 1] = aux
-                movimentos += 1
-    return comparacoes, movimentos
+    return v
+
 
 def medir_tempo(array):
     tempo_inicial = time.time()
-    comparacoes, movimentos = bubbleSort(array)
+    ordenado = bubbleSort(array)
     tempo_final = time.time()
-    return tempo_final - tempo_inicial, comparacoes, movimentos
+    return tempo_final - tempo_inicial,ordenado
 
 def plotar_grafico(tamanhos, ordens, titulo, legendas):
     for i in range(len(ordens)):
@@ -30,24 +27,27 @@ def plotar_grafico(tamanhos, ordens, titulo, legendas):
     plt.grid(True)
     plt.legend()
 
-<<<<<<< Updated upstream
+def criar_arquivo(nome, array):
+    nomef = f"{nome}.txt"
+    with open(nomef, 'w') as file:
+        for elemento in array:
+            file.write(str(elemento) + '\n')
+
 tamanhos = [100, 1000, 10000]
 ordens = [[], [], []]
 legendas = ['Crescente', 'Decrescente', 'Aleatória']
-=======
-sizes = [100, 1000, 10000]
-execution_times = [[], [], []]
-labels = ['Crescente', 'Decrescente', 'Aleatória']
->>>>>>> Stashed changes
 
 for tamanho in tamanhos:
     array_cresc = list(range(tamanho))
     array_decresc = list(range(tamanho, 0, -1))
     array_random = random.sample(range(tamanho * 10), tamanho)
-
-    tempo_asc, _, _ = medir_tempo(array_cresc)
-    tempo_desc, _, _ = medir_tempo(array_decresc)
-    tempo_rand, _, _ = medir_tempo(array_random)
+    array_ordenado = []
+    tempo_asc,array_ordenado = medir_tempo(array_cresc)
+    criar_arquivo(f"{len(array_ordenado)}cresc", array_ordenado)
+    tempo_desc,array_ordenado = medir_tempo(array_decresc)
+    criar_arquivo(f"{len(array_ordenado)}decresc", array_ordenado)
+    tempo_rand,array_ordenado = medir_tempo(array_random)
+    criar_arquivo(f"{len(array_ordenado)}random", array_ordenado)
 
     ordens[0].append(tempo_asc)
     ordens[1].append(tempo_desc)
--- periodo-6/ESTRUTURA DE DADOS II/sorting-algorithms/selectionSort.py
+++ periodo-6/ESTRUTURA DE DADOS II/sorting-algorithms/selectionSort.py
@@ -3,26 +3,22 @@ import random
 import matplotlib.pyplot as plt
 
 def selectionSort(v):
-    comparacoes = 0
-    movimentos = 0
     for i in range(len(v)):
         menor = i
         for j in range(i+1, len(v)):
-            comparacoes += 1
             if v[j] < v[menor]:
                 menor = j
         if menor != i:
-            movimentos += 1
             troca = v[i]
             v[i] = v[menor]
             v[menor] = troca
-    return comparacoes, movimentos
+    return v
 
 def medir_tempo(array):
     tempo_inicial = time.time()
-    comparacoes, movimentos = selectionSort(array)
+    ordenado = selectionSort(array)
     tempo_final = time.time()
-    return tempo_final - tempo_inicial, comparacoes, movimentos
+    return tempo_final - tempo_inicial,ordenado
 
 def plotar_grafico(tamanhos, ordens, titulo, legendas):
     for i in range(len(ordens)):
@@ -33,24 +29,27 @@ def plotar_grafico(tamanhos, ordens, titulo, legendas):
     plt.grid(True)
     plt.legend()
 
-<<<<<<< Updated upstream
+def criar_arquivo(nome, array):
+    nomef = f"{nome}.txt"
+    with open(nomef, 'w') as file:
+        for elemento in array:
+            file.write(str(elemento) + '\n')
+
 tamanhos = [100, 1000, 10000]
 ordens = [[], [], []]
 legendas = ['Crescente', 'Decrescente', 'Aleatória']
-=======
-sizes = [100, 1000, 10000]
-execution_times = [[], [], []]
-labels = ['Crescente', 'Decrescente', 'Aleatória']
->>>>>>> Stashed changes
 
 for tamanho in tamanhos:
     array_cresc = list(range(tamanho))
     array_decresc = list(range(tamanho, 0, -1))
     array_random = random.sample(range(tamanho * 10), tamanho)
-
-    tempo_asc, _, _ = medir_tempo(array_cresc)
-    tempo_desc, _, _ = medir_tempo(array_decresc)
-    tempo_rand, _, _ = medir_tempo(array_random)
+    array_ordenado = []
+    tempo_asc,array_ordenado = medir_tempo(array_cresc)
+    criar_arquivo(f"{len(array_ordenado)}cresc", array_ordenado)
+    tempo_desc,array_ordenado = medir_tempo(array_decresc)
+    criar_arquivo(f"{len(array_ordenado)}decresc", array_ordenado)
+    tempo_rand,array_ordenado = medir_tempo(array_random)
+    criar_arquivo(f"{len(array_ordenado)}random", array_ordenado)
 
     ordens[0].append(tempo_asc)
     ordens[1].append(tempo_desc)
